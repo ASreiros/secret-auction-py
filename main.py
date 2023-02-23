0f6e3b359b27d1153d8c7f255efb0be8a50af29b
Anton Sokolkin <97021906+ASreiros@users.noreply.github.com>
2023-02-23 17:15:08 +0200

    Auction working. TODO: clear console

--- main.py
+++ main.py
@@ -12,13 +12,24 @@ while bid_flag is False:
         bid_float = True
 
     except:
-        print("Your bid is not float. It will be ignored")
         bid_float = False
 
-    if bid_float is True:
+    if bid_float is True and float(bid)>0:
         bidders[name] = float(bid)
+    else:
+        print("Your bid is not a positive amount of money. It will be ignored")
 
     more = input('Are there any other bidders? Type "yes" or "no"').lower()
     if more != "yes":
         bid_flag = True
 
+if bidders:
+    good_bid = 0
+    bid_name = ""
+    for key in bidders:
+        if bidders[key] > good_bid:
+            good_bid = bidders[key]
+            bid_name = key
+    print(f"The winner is {bid_name} with a bid of ${good_bid}")
+else:
+    print("There are no bidders with valid bids.")
